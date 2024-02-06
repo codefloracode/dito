ca9feaaa423293f28149083b30f27e850307aa02
Flora Martins <110993474+codefloracode@users.noreply.github.com>
2024-02-06 16:06:14 -0300

    Add files via upload

--- templates/index.php
+++ templates/index.php
@@ -6,34 +6,55 @@ Template Name: Home
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-        <div class="cover" style="background-image: url('<?php echo get_field('cover_img'); ?>')">
-            <div class="description"><?php echo get_field('description'); ?></div>
+        <div id="primary-container" class="scrollable-content">
+            <div class="scroll-bullets"></div>
+            <div class="cover post-cover" style="background-image: url('<?php echo get_field('cover_img'); ?>')">
+                <?php if(get_field('cover_video')): ?>
+                    <div class="videowrap">
+                        <video autoplay mute loop><source src="<?php echo get_field('cover_video'); ?>" type="video/mp4"></video>
+                    </div>
+                    <div class="videowrap mobile">
+                        <video autoplay mute loop><source src="<?php echo get_field('cover_video_mobile'); ?>" type="video/mp4"></video>
+                    </div>
+                <?php endif; ?>
+                <div class="description"><?php echo get_field('description'); ?></div>
+            </div>
+
+            <?php 
+            
+            $args = array(
+                'post_type' => 'post', // Fetch posts
+                'posts_per_page' => 999, // Number of posts to display
+            );
+            
+            $custom_query = new WP_Query($args);
+
+            if ($custom_query->have_posts()) :
+                while ($custom_query->have_posts()) :
+                    $custom_query->the_post();
+                    ?>
+
+                <article id="post-<?php the_ID(); ?>">
+                    <a href="<?php the_permalink(); ?>" class="post-cover" style="background-image: url('<?php echo get_field('cover_img'); ?>')">
+                        <?php if(get_field('cover_video')): ?>
+                            <div class="videowrap">
+                                <video autoplay mute loop><source src="<?php echo get_field('cover_video'); ?>" type="video/mp4"></video>
+                            </div>
+                            <div class="videowrap mobile">
+                                <video autoplay mute loop><source src="<?php echo get_field('cover_video_mobile'); ?>" type="video/mp4"></video>
+                            </div>
+                        <?php endif; ?>
+                        <h2 class="entry-title"><?php the_title(); ?></h2>
+                    </a>
+                </article>
+
+            <?php
+                endwhile;
+                wp_reset_postdata(); // Reset the query
+            else :
+                echo 'No posts found.';
+            endif; ?>
         </div>
-
-        <?php 
-        
-        $args = array(
-            'post_type' => 'post', // Fetch posts
-            'posts_per_page' => 999, // Number of posts to display
-        );
-        
-        $custom_query = new WP_Query($args);
-
-        if ($custom_query->have_posts()) :
-            while ($custom_query->have_posts()) :
-                $custom_query->the_post();
-                ?>
-
-            <article id="post-<?php the_ID(); ?>" class="post-cover" style="background-image: url('<?php echo get_field('cover_img'); ?>')">
-                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            </article>
-
-        <?php
-            endwhile;
-            wp_reset_postdata(); // Reset the query
-        else :
-            echo 'No posts found.';
-        endif; ?>
 
     </main>
 </div>
