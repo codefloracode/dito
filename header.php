ba7186512bd88d8680fa7a6585ad1ebe16e19ef7
codefloracode <codefloracode@gmail.com>
2024-03-14 10:39:47 -0300

    add new thumb

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/x-icon" href="http://dito.pro/wp-content/themes/dito/assets/imgs/logo-thumb.ico">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
